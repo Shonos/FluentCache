ef4838be56d2e9886a2a4f1f56ae3919c9c50db8
Shaun Alonzo <shaunalonzodev@gmail.com>
2024-08-26 09:18:43 +0800

    feat: create with on cache miss with tests

---
+++ Shonos.FluentCache/FluentCache.cs
@@ -0,0 +1,138 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using Shonos.FluentCache.Abstractions;
+using System.Text.Json;
+
+namespace Shonos.FluentCache
+{
+    /// <summary>
+    /// Implementation of IFluentCache
+    /// OnCacheMiss sets the type
+    /// </summary>
+    public class FluentCache : IFluentCache
+    {
+        private readonly IDistributedCache _cache;
+
+        public FluentCache(IDistributedCache cache)
+        {
+            _cache = cache;
+        }
+
+        public IFluentCache<T> CreateWithOnCacheMiss<T>(Func<Task<T>> action) => new FluentCache<T>(_cache, action);
+
+        public Task RemoveAsync(string key, CancellationToken cancellationToken = default) => _cache.RemoveAsync(key, cancellationToken);
+    }
+    /// <summary>
+    /// Implementation of a typed IFluentCache using ICache
+    /// </summary>
+    public class FluentCache<T> : IFluentCache<T>
+    {
+        private readonly IDistributedCache _cache;
+        private readonly Func<Task<T>>? _onCacheMiss;
+        private Func<T, bool>? _cacheIfPredicate;
+
+        private TimeSpan? _absoluteExpirationRelativeToNow;
+        private DateTimeOffset? _absoluteExpiration;
+        private TimeSpan? _slidingExpiration;
+
+        public FluentCache(IDistributedCache cache, Func<Task<T>> onCacheMiss)
+        {
+            _cache = cache;
+            _onCacheMiss = onCacheMiss;
+            _absoluteExpirationRelativeToNow = TimeSpan.FromMinutes(5); // default cache expiration is 5 minutes 
+        }
+
+        public IFluentCache<T> CacheIf(Func<T, bool> predicate)
+        {
+            _cacheIfPredicate = predicate;
+            return this;
+        }
+
+        public IFluentCache<T> SetAbsoluteExpirationRelativeToNow(TimeSpan duration)
+        {
+            // set other time variables to null to prevent conflict 
+            _absoluteExpiration = null;
+            _absoluteExpirationRelativeToNow = duration;
+            _slidingExpiration = null;
+            return this;
+        }
+
+        public IFluentCache<T> SetAbsoluteExpiration(DateTimeOffset offset)
+        {
+            // set other time variables to null to prevent conflict 
+            _absoluteExpiration = offset;
+            _absoluteExpirationRelativeToNow = null;
+            _slidingExpiration = null;
+            return this;
+        }
+
+        public IFluentCache<T> SetSlidingExpiration(TimeSpan duration)
+        {
+            // set other time variables to null to prevent conflict 
+            _absoluteExpiration = null;
+            _absoluteExpirationRelativeToNow = null;
+            _slidingExpiration = duration;
+            return this;
+        }
+
+        private async Task<T?> GetAsync<TValue>(string key, CancellationToken cancellationToken = default)
+        {
+            var resultInString = string.Empty;
+
+            resultInString = await _cache.GetStringAsync(key, cancellationToken).ConfigureAwait(false);
+
+            return string.IsNullOrWhiteSpace(resultInString)
+                ? default
+                : JsonSerializer.Deserialize<T>(resultInString);
+        }
+
+        public async Task<T?> GetAsync(string key, CancellationToken cancellationToken = default)
+        {
+            var cachedData = await GetAsync<T>(key, cancellationToken).ConfigureAwait(false);
+
+            if (cachedData == null && _onCacheMiss != null)
+            {
+                var result = await _onCacheMiss();
+                if (result != null && (_cacheIfPredicate == null || _cacheIfPredicate(result)))
+                {
+                    await SetAsync(key, result,
+                        absoluteExpirationRelativeToNow: _absoluteExpirationRelativeToNow,
+                        absoluteExpiration: _absoluteExpiration,
+                        slidingExpiration: _slidingExpiration,
+                        cancellationToken: cancellationToken).ConfigureAwait(false);
+                }
+                return result;
+            }
+
+            return cachedData;
+        }
+
+        public async Task<bool> SetAsync(string key, T value,
+            DateTimeOffset? absoluteExpiration = default,
+            TimeSpan? slidingExpiration = default,
+            TimeSpan? absoluteExpirationRelativeToNow = default,
+            CancellationToken cancellationToken = default)
+        {
+            var valueInString = JsonSerializer.Serialize(value);
+
+            try
+            {
+                await _cache.SetStringAsync(
+                    key,
+                    valueInString,
+                    new DistributedCacheEntryOptions
+                    {
+                        AbsoluteExpiration = absoluteExpiration,
+                        AbsoluteExpirationRelativeToNow = absoluteExpirationRelativeToNow,
+                        SlidingExpiration = slidingExpiration
+                    },
+                    cancellationToken).ConfigureAwait(false);
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ tests/Shonos.FluentCache.Tests/FluentCacheTests.cs
@@ -0,0 +1,111 @@
+using FluentAssertions;
+using Microsoft.Extensions.Caching.Distributed;
+using Moq;
+using System.Text;
+using System.Text.Json;
+
+namespace Shonos.FluentCache.Tests
+{
+    public class FluentCacheTests
+    {
+        private readonly Mock<IDistributedCache> _mockCache;
+        private readonly FluentCache _fluentCache;
+
+        public FluentCacheTests()
+        {
+            _mockCache = new Mock<IDistributedCache>();
+            _fluentCache = new FluentCache(_mockCache.Object);
+        }
+
+        [Fact]
+        public void CreateWithOnCacheMiss_ShouldReturnFluentCacheOfT()
+        {
+            // Arrange
+            Func<Task<int>> action = () => Task.FromResult(42);
+
+            // Act
+            var result = _fluentCache.CreateWithOnCacheMiss(action);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.IsType<FluentCache<int>>(result);
+        }
+
+        [Fact]
+        public async Task RemoveAsync_ShouldCallRemoveAsyncOnCache()
+        {
+            // Arrange
+            var key = "testKey";
+
+            // Act
+            await _fluentCache.RemoveAsync(key);
+
+            // Assert
+            _mockCache.Verify(c => c.RemoveAsync(key, It.IsAny<CancellationToken>()), Times.Once);
+        }
+
+        [Fact]
+        public async Task GetAsync_CacheMiss_ReturnsValueFromOnCacheMiss()
+        {
+            // Arrange
+            var cacheKey = "test_key";
+
+            var expectedObjectValue = new SampleCachedObject()
+            {
+                Value1 = "testvalue",
+            };
+
+            // Setup OnCacheMiss function
+            Func<Task<SampleCachedObject>> onCacheMiss = () => Task.FromResult(expectedObjectValue);
+
+            var fluentCache = new FluentCache(_mockCache.Object);
+            var typedFluentCache = fluentCache.CreateWithOnCacheMiss(onCacheMiss);
+            // Act
+            var result = await typedFluentCache.GetAsync(cacheKey);
+
+            // Assert
+            result.Should().BeEquivalentTo(expectedObjectValue);
+
+            // Verify that IDisitributedCache called Set Async
+            _mockCache.Verify(c => c.SetAsync(cacheKey, It.IsAny<byte[]>(), It.IsAny<DistributedCacheEntryOptions>(), It.IsAny<CancellationToken>()), Times.Once);
+        }
+
+        [Fact]
+        public async Task GetAsync_CacheHit_ReturnsValueFromCache()
+        {
+            // Arrange
+            var cacheKey = "test_key";
+
+            var expectedObjectValue = new SampleCachedObject()
+            {
+                Value1 = "testvalue",
+            };
+
+            // Setup OnCacheMiss function
+            Func<Task<SampleCachedObject>> onCacheMiss = () => Task.FromResult(expectedObjectValue);
+
+            var fluentCache = new FluentCache(_mockCache.Object);
+            var typedFluentCache = fluentCache.CreateWithOnCacheMiss(onCacheMiss);
+
+            // Simulate cache hit
+            string expectedObjectValueAsJson = JsonSerializer.Serialize(expectedObjectValue);
+            var expectedObjectValueAsBytes = Encoding.UTF8.GetBytes(expectedObjectValueAsJson);
+            _mockCache.Setup(c => c.GetAsync(cacheKey, It.IsAny<CancellationToken>())).ReturnsAsync(expectedObjectValueAsBytes);
+
+            // Act
+            var result = await typedFluentCache.GetAsync(cacheKey);
+
+            // Assert
+            result.Should().BeEquivalentTo(expectedObjectValue);
+
+            // Verify that IDistributedCache never called SetAsync
+            _mockCache.Verify(c => c.SetAsync(cacheKey, It.IsAny<byte[]>(), It.IsAny<DistributedCacheEntryOptions>(), It.IsAny<CancellationToken>()), Times.Never);
+        }
+
+        protected class SampleCachedObject
+        {
+            public string Value1 { get; set; }
+            public string? Value2 { get; set; }
+        }
+    }
+}
